de7ffd56e90e64571faba51aee925d7d657c8d33
RenoFar <renaudfaraci@gmail.com>
2020-10-14 14:13:11 +0200

    Structuration

--- Main.py
+++ Main.py
@@ -3,6 +3,17 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-request = requests.get("https://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html")
-page = request.content
-soup = BeautifulSoup(request.text, "html.parser")
+# Extract
+page_selection = requests.get("http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html")
+
+# Transform
+page_html = BeautifulSoup(page_selection.text, "html.parser")
+
+product_page_url = []
+for page_url in page_html.find_all('a'):
+    product_page_url.append(page_url.get('href'))
+
+
+# Load
+""" print(page_html.prettify())"""
+print(product_page_url)
