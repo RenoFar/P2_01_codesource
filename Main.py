1c58d92fa14ac830a30ebd53676b97387c9663ff
RenoFar <renaudfaraci@gmail.com>
2020-10-15 11:59:15 +0200

    mise en forme des données avec BS4

--- Main.py
+++ Main.py
@@ -4,18 +4,47 @@ import requests
 from bs4 import BeautifulSoup
 
 # Extract
-request = requests.get("http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html")
-page_selection = request.text
+url = "http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html"
+request = requests.get(url)
+if request.ok:
 
-# Transform
-page_html = BeautifulSoup(page_selection, "html.parser")
-""" product_page_url = []
-for page_url in page_html.find_all('a'):
-    product_page_url.append(page_url.get('href'))"""
+    # Transform
+    page_html = BeautifulSoup(request.text, "html.parser")
 
-# Load
-""" print(page_html.prettify())"""
-with open('P2_01_extract.csv', 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['product_page_url', request.url])
-csv_file.close()
+    product_page_url = url
+    title = page_html.find('h1')
+
+    tr_dic = {}
+    for trs in page_html.findAll('tr'):
+        ths = trs.findAll('th')
+        tds = trs.findAll('td')
+        tr_dic[ths[0].string] = tds[0].string
+
+    universal_product_code = tr_dic['UPC']
+    price_including_tax = tr_dic['Price (incl. tax)']
+    price_excluding_tax = tr_dic['Price (excl. tax)']
+    number_available = tr_dic['Availability']
+    review_rating = tr_dic['Number of reviews']
+   
+    image_url = page_html.find('img')['src']
+
+    product_description = page_html.find('div', attrs={'id':'product_description', 'class':'sub-header'}).next_element
+    category = page_html.find('ul', attrs={'class':'breadcrumb'}).find_all('a')[-1]
+    print(category)
+    """
+    page_info = [product_page_url, universal_product_code, title.text, price_including_tax,
+                 price_excluding_tax, number_available, product_description.text, category,
+                 review_rating, image_url]
+
+    print(page_info)
+    """
+    
+   # Load
+        # print(page_html.prettify())
+    with open('P2_01_extract.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow('product_page_url', 'universal_product_code', 'title', 'price_including_tax',
+                             'price_excluding_tax', 'number_available', 'product_description', 'category',
+                             'review_rating', 'image_url')
+        # fonction add info in new line
+    # csv_file.close()
