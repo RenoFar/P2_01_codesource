d62ad933537609b7ebd497562d77fa99f3aa6fb4
RenoFar <renaudfaraci@gmail.com>
2020-10-20 14:00:57 +0200

    extraction d'une page terminée

--- Main.py
+++ Main.py
@@ -38,9 +38,8 @@ if request.ok:
     # Load
     with open('P2_01_extract.csv', 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
-        Header = ['product_page_url', 'universal_product_code', 'title', 'price_including_tax',
+        Header = ['product_page_url', 'universal_ product_code (upc)', 'title', 'price_including_tax',
                  'price_excluding_tax', 'number_available', 'product_description', 'category',
                  'review_rating', 'image_url']
         csv_writer.writerow(Header)
         csv_writer.writerow(page_info)
-    # csv_file.close()
