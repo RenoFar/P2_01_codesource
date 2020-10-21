0ac2c9c22210faebf23b743fd7af19004bc54b21
RenoFar <renaudfaraci@gmail.com>
2020-10-21 18:09:05 +0200

    Utilisation de la variable book_category pour la creation du csv. test hors ligne de la liste des données

--- Main.py
+++ Main.py
@@ -39,17 +39,18 @@ def transform_info(url_chosen):
     return  page_info
 
 
-def create_csv(rows):
+def create_csv(rows, name_cat):
     header = list(rows.keys())
-    with open('P2_01_extract.csv', 'a', encoding='utf-8', newline='') as csv_file:
+
+    with open('P2_01_' + str(name_cat) + '.csv', 'a', encoding='utf-8', newline='') as csv_file:
         csv_writer = csv.DictWriter(csv_file, header)
-        if os.stat('P2_01_extract.csv').st_size == 0:
+        if os.stat('P2_01_' + str(name_cat) + '.csv').st_size == 0:
             csv_writer.writeheader()
         csv_writer.writerow(rows)
 
 
-def listing_url(url_site, book_category):
-    url_category = url_site + 'catalogue/category/books/' + book_category + 'index.html'
+def listing_url(url_site, book_cat):
+    url_category = url_site + 'catalogue/category/books/' + book_cat + '/index.html'
     url_list = [url_site + 'catalogue/' + '/'.join(u.a['href'].split('/')[3:-1])
                 for u in BeautifulSoup(extract_url(url_category).text, "html.parser").find_all('h3')]
 
@@ -58,32 +59,49 @@ def listing_url(url_site, book_category):
     if book_numbers >= 20:
         if book_numbers % 20 == 0:
             for i in range(2, int(book_numbers//20)+1):
-                url_category = url_site + 'catalogue/category/books/' + book_category + 'page-' + str(i) + '.html'
+                url_category = url_site + 'catalogue/category/books/' + book_cat + '/page-' + str(i) + '.html'
                 url_list = url_list + [url_site + 'catalogue/' +
                                        '/'.join(u.a['href'].split('/')[3:-1])
                                        for u in BeautifulSoup(extract_url(url_category).text,
                                                               "html.parser").find_all('h3')]
         else:
             for i in range(2, int((book_numbers//20)+1)+1):
-                url_category = url_site + 'catalogue/category/books/' + book_category + 'page-' + str(i) + '.html'
+                url_category = url_site + 'catalogue/category/books/' + book_cat + '/page-' + str(i) + '.html'
                 url_list = url_list + [url_site + 'catalogue/' +
                                        '/'.join(u.a['href'].split('/')[3:-1])
                                        for u in BeautifulSoup(extract_url(url_category).text,
                                                               "html.parser").find_all('h3')]
     return url_list
 
-def listing_category():
+#def listing_category():
 
-def Writing_data(url_lists):
+def Writing_data(url_lists, books_cat):
     for u in range(len(url_lists)):
-        pages_data = transform_info(url_lists[u])
-        create_csv(pages_data)
+        pages_data = urls[u]
+        """pages_data = transform_info(url_lists[u])"""
+        create_csv(pages_data, books_cat)
 
 
 #choix et requete des pages
 url_site = 'http://books.toscrape.com/'
-book_category = 'sequential-art_5/'
-urls = listing_url(url_site, book_category)
+book_category = 'sequential-art_5'
 
+"""urls = listing_url(url_site, book_category)"""
+urls =({'product_page_url': '1', 'universal_ product_code (upc)': '2',
+        'title': '3', 'price_including_tax': '4','price_excluding_tax': '5',
+        'number_available': '6', 'product_description': '7', 'category':'8',
+        'review_rating': '9', 'image_url': '10'},
+       {'product_page_url': '11', 'universal_ product_code (upc)': '12',
+        'title': '13', 'price_including_tax': '14', 'price_excluding_tax': '15',
+        'number_available': '16', 'product_description': '17', 'category': '18',
+        'review_rating': '19', 'image_url': '20'},
+       {'product_page_url': '21', 'universal_ product_code (upc)': '2',
+        'title': '23', 'price_including_tax': '24', 'price_excluding_tax': '25',
+        'number_available': '26', 'product_description': '27', 'category': '28',
+        'review_rating': '29', 'image_url': '30'},
+       {'product_page_url': '31', 'universal_ product_code (upc)': '32',
+        'title': '33', 'price_including_tax': '34', 'price_excluding_tax': '35',
+        'number_available': '36', 'product_description': '37', 'category': '38',
+        'review_rating': '39', 'image_url': '40'})
 #mise en forme et écriture des données
-Writing_data(urls)
+Writing_data(urls, book_category)
